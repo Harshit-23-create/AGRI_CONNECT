6d12bd3be141b0dd88c82fa6bfd25b6c1f25aeb5
Harshit-23-create <harshit123412@gmail.com>
2026-08-02 23:23:44 +0530

    fix: remove hardcoded dns which breaks render deployments

--- agri-connect/server/config/db.js
+++ agri-connect/server/config/db.js
@@ -1,6 +1,4 @@
 const mongoose = require('mongoose');
-const dns = require('dns');
-dns.setServers(['8.8.8.8', '8.8.4.4']);
 
 const connectDB = async (retries = 5) => {
   for (let i = 0; i < retries; i++) {
